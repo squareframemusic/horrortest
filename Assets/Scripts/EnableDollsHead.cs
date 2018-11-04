f74d7ea0b98ed236c672b7394e2305e822e31dc9
DESKTOP-68MFNK8\Az <adam.scottmcguinness@gmail.com>
2018-11-04 13:14:08 +1100

    Dolls head in front of camera now active.

---
+++ Assets/Scripts/EnableDollsHead.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDollsHead : MonoBehaviour {
+
+    public GameObject dollHead;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTriggerEnter(Collider other)
+    {
+        //enables the dolls head in front of the camera.
+        dollHead.SetActive(true);
+        Debug.Log("collided with doll head activator");
+    }
+}
