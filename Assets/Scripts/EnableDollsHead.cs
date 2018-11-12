d5c600d364d611e5ae149be6993d272d3ed8f943
DESKTOP-68MFNK8\Az <adam.scottmcguinness@gmail.com>
2018-11-12 21:51:01 +1100

    Sound Pitch Logo added to ending.

--- Assets/Scripts/EnableDollsHead.cs
+++ Assets/Scripts/EnableDollsHead.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnableDollsHead : MonoBehaviour {
 
     public GameObject dollHead;
+    public GameObject character;
+    public GameObject endDestination;
+    public GameObject logo;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +26,27 @@ public class EnableDollsHead : MonoBehaviour {
         //turns on glitchy camera effect.
         GameObject.Find("FirstPersonCharacter").GetComponent<GlitchEffect>().enabled = true;
 
+        StartCoroutine(EndGame());
+
         Debug.Log("collided with doll head activator");
+    }
+
+    IEnumerator EndGame()
+    {
+        //waits for this amount of time.
+        yield return new WaitForSeconds(1f);
+
+        //turns off the dolls head.
+        dollHead.SetActive(false);
+
+        //moves player to end of game prison.
+        character.transform.position = endDestination.transform.position;
+
+        //turns off glitchy visual effect.
+        GameObject.Find("FirstPersonCharacter").GetComponent<GlitchEffect>().enabled = false;
+
+        yield return new WaitForSeconds(2.5f);
+        //enables logo.
+        logo.SetActive(true);
     }
 }
