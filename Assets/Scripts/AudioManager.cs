fbac87430dcfed01bc35a25978637b06cdd72347
DESKTOP-68MFNK8\Az <adam.scottmcguinness@gmail.com>
2018-11-12 14:00:46 +1100

    Colliders and rigidbodies set up on all trollies, cups and hospital beds (except the last one that you never get to). Collision detection utility added to all. CollisionAudio script has a dropdown menu to determin what type of object it is attached to. Preliminary rough fmod events set up.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -31,9 +31,36 @@ public class AudioManager : MonoBehaviour
         FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Footsteps", firstPersonFoot.transform.position);
     }
 
+
+    //currently the methods TrollyCollision, CupCollision and HospitalBedCollision are using the same
+    //fmod parameter. This may need to be changed after testing.
     public void TrollyCollision(float vel, Vector3 position)
     {
-        var trollySound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/HospitalEquipment 01");
+        var trollySound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/TrolleyCollide");
+        FMOD.Studio.ParameterInstance collisionVel;
+
+        trollySound.getParameter("TrolleyCollisionVelocity", out collisionVel);
+        collisionVel.setValue(vel);
+        trollySound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(position));
+        trollySound.start();
+        trollySound.release(); //destroys the instance from memory once audio has played. Prevents memory leak.
+    }
+
+    public void CupCollision(float vel, Vector3 position)
+    {
+        var trollySound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/CupCollide");
+        FMOD.Studio.ParameterInstance collisionVel;
+
+        trollySound.getParameter("collisionVelocity", out collisionVel);
+        collisionVel.setValue(vel);
+        trollySound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(position));
+        trollySound.start();
+        trollySound.release(); //destroys the instance from memory once audio has played. Prevents memory leak.
+    }
+
+    public void HospitalBedCollision(float vel, Vector3 position)
+    {
+        var trollySound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/HospitalBedCollide");
         FMOD.Studio.ParameterInstance collisionVel;
 
         trollySound.getParameter("collisionVelocity", out collisionVel);
---
+++ Assets/Scripts/CollisionAudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAudio : MonoBehaviour {
+
+    public enum objectList
+    {
+        trolley,
+        cup,
+        hospitalBed
+    }
+
+    public objectList objectType;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        //if statement sets a gate to prevent smaller collisions from occurring.
+        if (collision.relativeVelocity.magnitude > 0.50f)
+        {
+            if (objectType == objectList.trolley)
+            {
+                AudioManager.instance.TrollyCollision(collision.relativeVelocity.magnitude, this.transform.position);
+            }
+            if (objectType == objectList.cup)
+            {
+                AudioManager.instance.CupCollision(collision.relativeVelocity.magnitude, this.transform.position);
+            }
+            if (objectType == objectList.hospitalBed)
+            {
+                AudioManager.instance.HospitalBedCollision(collision.relativeVelocity.magnitude, this.transform.position);
+            }
+        }
+     
+    }
+}
